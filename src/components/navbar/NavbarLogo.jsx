2b8b36ef332989889dbeae0b77c755e4b6c58d7a
Sagor Majomder <sagormajomder@gmail.com>
2026-01-05 13:29:56 +0600

    refactor NavbarLogo component to use Link for navigation

--- src/components/navbar/NavbarLogo.jsx
+++ src/components/navbar/NavbarLogo.jsx
@@ -1,15 +1,18 @@
+import Link from 'next/link';
 import { FaLaptopCode } from 'react-icons/fa6';
 
 const NavbarLogo = () => {
   return (
-    <div className='flex items-center gap-3 text-gray-900 dark:text-white'>
+    <Link
+      href={'/'}
+      className='flex items-center gap-3 text-gray-900 dark:text-white'>
       <div className='text-primary'>
         <FaLaptopCode className='text-2xl' />
       </div>
       <h2 className='text-gray-900 dark:text-white text-lg font-bold leading-tight tracking-[-0.015em]'>
         Sagor Majomder
       </h2>
-    </div>
+    </Link>
   );
 };
 
